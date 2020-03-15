5333cbd1c791a675182abf5bc59a5974d1ff5b24
Samantha Dixon <sam.dixon@berkeley.edu>
2020-03-15 15:22:33 -0700

    only download light curves of spectroscopically confirmed Ias from the PS1 data set

--- download_ps1.py
+++ download_ps1.py
@@ -1,10 +1,13 @@
 import os
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
 TARGET_PATH = '/Users/samdixon/data/PS1_lightcurves'
 URL = 'https://archive.stsci.edu/hlsps/ps1cosmo/jones/lightcurves/'
+CAT_URL = 'https://archive.stsci.edu/hlsps/ps1cosmo/jones/' \
+          'hlsp_ps1cosmo_panstarrs_gpc1_all_multi_v1_snparams.txt'
 
 
 def get_urls(base_url=URL, ext='.dat'):
@@ -27,14 +30,31 @@ def download_to_file(dir, fname, url):
         f.write(r.text)
 
 
+def get_spec_conf_names(url=CAT_URL):
+    cat = pd.read_csv(CAT_URL, delim_whitespace=True, skiprows=1,
+                      names=['ID', 'RA', 'Dec', 'RAHost', 'DecHost', 'zHost',
+                             'NormSep', 'TDR', 'zSource', 'zSN', 'tpk', 'e_tpk',
+                             'x1', 'e_x1', 'c', 'e_c', 'mB', 'e_mB', 'logM',
+                             'e_logM', 'distcorr', 'PIa_PSNID', 'PIa_NN',
+                             'PIa_Fitprob', 'PIa_Galsnid'])
+    spec_conf = cat.PIa_PSNID==1.0
+    spec_conf &= cat.PIa_NN==1.0
+    spec_conf &= cat.PIa_Fitprob==1.0
+    spec_conf &= cat.PIa_Galsnid==1.0
+    return cat[spec_conf].ID
+
+
 def main():
     urls = get_urls()
     sn_names = [url.split('_')[4] for url in urls]
+    urls = dict(zip(sn_names, urls))
+    spec_conf_names = get_spec_conf_names()
     os.makedirs(TARGET_PATH, exist_ok=True)
-    for name, url in tqdm(zip(sn_names, urls)):
+    for name in tqdm(spec_conf_names):
         fname = '{}.dat'.format(name)
-        download_to_file(TARGET_PATH, fname, url)
+        download_to_file(TARGET_PATH, fname, urls[name])
 
 
 if __name__ == '__main__':
     main()
+
